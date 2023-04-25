6ede2710e8d2bbe9fdf6564bd8f586cf104a62a2
HamsDev <119873468+CeliaKlc@users.noreply.github.com>
2023-04-25 08:59:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const city = process.env.MY_CITY;
 const language = process.env.MY_LANGUAGE;
 
 console.log(
-  "I am " + name + ", wilder in " + city + ", and I love " + language + ".")
+  "I am " + name + ", wilder in " + city + ", and I love " + language + ".");
